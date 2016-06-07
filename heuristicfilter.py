df1ab3679ae20ee3958f0f418fece25adab6f5f0
Rohith Pillai <rohithkp@andrew.cmu.edu>
2016-06-07 16:26:54 +0400

    Modified a to fix all the bugs...

--- heuristicfilter.py
+++ heuristicfilter.py
@@ -2,14 +2,16 @@
 #Om Gum Ganapathaye Namo Namaha
 import progressbar
 
+""" Code complexity Work: O(n^2) """
+
 # read the filterwords
-fil = open("filterwords","w")
+fil = open("filterwords","r")
 
 line = fil.readline()
 filwrd = []
 while line:
-    if line[0] != '#':
-        filwrd += [line]
+    if line[0] != '#' and line[0] != '\n':
+        filwrd += [line.replace("\n","")]
     line = fil.readline()
 fil.close()
 
@@ -19,31 +21,42 @@ with open('Extracted_Noun_Verb_Phrases_wikipedia.txt') as f:
 f.close()
 
 #go through the NP and NPs
-fvnp = open("Extracted_Noun_Verb_Phrases_wikipedia.txt","w")
+fvnp = open("Extracted_Noun_Verb_Phrases_wikipedia.txt","r")
 
 # write to new file
 fout =  open("Cleaned_Norm_VPnNPs_wikipedia.txt","w")
+print lentot
+print filwrd
+##error()
 
 linevnp = fvnp.readline().lower()
-P = progressbar.ProgressBar(lentot)
+
+##P = progressbar.ProgressBar(lentot)
 count = 0
 
 while linevnp:
+
+    if linevnp[0] == '@':
+        fout.write(linevnp)
+        linevnp = fvnp.readline().lower()
+        continue
 
     removebool = False
     #remove words that are: is a prefix of words from filterwords
     for i in filwrd:
         spliti = i.split()
         lengthi = len(spliti)
-        if linevnp.split()[:lengthi] != spliti:
+        if linevnp.split()[:lengthi] == spliti:
             removebool = True
 
-    if !removebool:
-        fout.write(linevnp)
+
+    if not(removebool):
+        fout.write(linevnp.replace("=== ","").replace("== ",""))
+
 
 
     linevnp = fvnp.readline().lower()
-    P.update(count)
+##    P.update(count)
     count +=1
 
 fout.close()
